5a8109a228d62eff389d794627890b9c384b7360
Kyle Begeman <kylebegeman@gmail.com>
2019-01-10 23:27:07 -0800

    Initial Commit

---
+++ Beam/Beam.h
@@ -0,0 +1,19 @@
+//
+//  Beam.h
+//  Beam
+//
+//  Created by Kyle Begeman on 1/10/19.
+//  Copyright © 2019 Kyle Begeman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Beam.
+FOUNDATION_EXPORT double BeamVersionNumber;
+
+//! Project version string for Beam.
+FOUNDATION_EXPORT const unsigned char BeamVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Beam/PublicHeader.h>
+
+
